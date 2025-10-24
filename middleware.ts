beed1277b1218d430ce6cc0d5f2de2e2d12d9efa
Ara_Mansouri <140816682+Ara-Mansouri@users.noreply.github.com.>
2025-10-24 18:34:47 +0330

    Fix : middleware Issue

--- middleware.ts
+++ middleware.ts
@@ -5,16 +5,16 @@ import type { NextRequest } from "next/server";
 export function middleware(req: NextRequest) {
   const access = req.cookies.get("accessToken")?.value;
   const { pathname } = req.nextUrl;
-  const lowerPath = pathname.toLowerCase(); 
- console.log("🔥 MIDDLEWARE FIRED:", req.nextUrl.pathname);
-  if (!access && lowerPath.startsWith("/dashboard")) {
-    const url = new URL("/auth/login", req.url);
+  // const lowerPath = pathname.toLowerCase(); 
+
+  if (!access && pathname.startsWith("/dashboard")) {
+    const url = new URL("/auth/Login", req.url);
     return NextResponse.redirect(url);
   }
 
   if (
     access &&
-    (lowerPath.startsWith("/auth/login") || lowerPath.startsWith("/auth/register"))
+    (pathname.startsWith("/auth/Login") || pathname.startsWith("/auth/register"))
   ) {
     const url = new URL("/dashboard", req.url);
     return NextResponse.redirect(url);
