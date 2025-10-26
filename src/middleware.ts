3061d1451cb335b010d09630a83f2e576064446c
Ara_Mansouri <140816682+Ara-Mansouri@users.noreply.github.com.>
2025-10-26 23:55:38 +0330

    Fix : Middleware Wrong Route

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,24 +3,20 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
-  const access = req.cookies.get("accessToken")?.value;
-  // const reset  = req.cookies.get("resetEmail")?.value;
+  const accessToken = req.cookies.get("accessToken")?.value;
+  const refreshToken = req.cookies.get("refreshToken")?.value;
+
   const { pathname } = req.nextUrl;
 
   
-  if (!access && pathname.startsWith("/dashboard")) {
+  if ( (!accessToken && !refreshToken) && pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/auth/login", req.url));
   }
 
   
-  if (access && pathname.startsWith("/auth")) {
+  if ((accessToken || refreshToken) && pathname.startsWith("/auth")) {
     return NextResponse.redirect(new URL("/dashboard", req.url));
   }
-
-  
-  // if (!reset && pathname.startsWith("/auth/reset-password")) {
-  //   return NextResponse.redirect(new URL("/auth/forgot-password", req.url));
-  // }
 
   return NextResponse.next();
 }
