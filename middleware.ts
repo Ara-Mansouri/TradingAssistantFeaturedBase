112cf9aa1572c0cacf333a421a5b36cc97bd51eb
Ara_Mansouri <140816682+Ara-Mansouri@users.noreply.github.com.>
2025-10-26 07:17:16 +0330

    Feat : Fix MiddleWare

--- middleware.ts
+++ middleware.ts
@@ -1,31 +1,30 @@
-// src/app/middleware.ts
+// src/middleware.ts
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   const access = req.cookies.get("accessToken")?.value;
+  // const reset  = req.cookies.get("resetEmail")?.value;
   const { pathname } = req.nextUrl;
-  // const lowerPath = pathname.toLowerCase(); 
 
+  
   if (!access && pathname.startsWith("/dashboard")) {
-    const url = new URL("/auth/Login", req.url);
-    return NextResponse.redirect(url);
+    return NextResponse.redirect(new URL("/auth/login", req.url));
   }
 
-  if (
-    access &&
-    (pathname.startsWith("/auth/Login") || pathname.startsWith("/auth/register"))
-  ) {
-    const url = new URL("/dashboard", req.url);
-    return NextResponse.redirect(url);
+  
+  if (access && pathname.startsWith("/auth")) {
+    return NextResponse.redirect(new URL("/dashboard", req.url));
   }
+
+  
+  // if (!reset && pathname.startsWith("/auth/reset-password")) {
+  //   return NextResponse.redirect(new URL("/auth/forgot-password", req.url));
+  // }
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: [
-    "/dashboard/:path*",
-    "/auth/:path*",
-  ],
+  matcher: ["/dashboard/:path*", "/auth/:path*"],
 };
