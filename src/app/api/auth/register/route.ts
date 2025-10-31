770a1b4ed1da77ee6cb81c09c68751b1bf0e5f01
Ara_Mansouri <140816682+Ara-Mansouri@users.noreply.github.com.>
2025-10-31 19:35:09 +0330

    Fix : register Bug fix

--- src/app/api/auth/register/route.ts
+++ src/app/api/auth/register/route.ts
@@ -22,7 +22,7 @@ export async function POST(req: Request)
      
     }
 
-    
+    return NextResponse.json(data, { status: Results.status });
     } 
    catch (err) 
    {
--- src/features/auth/hooks/useRegister.ts
+++ src/features/auth/hooks/useRegister.ts
@@ -2,24 +2,25 @@
 
 import { useMutation } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";   
-import { registerApi , RegisterPayload } from "../services/auth.api";
+import { registerApi, RegisterPayload } from "../services/auth.api";
+import { useLocale } from "next-intl"; 
 
+export function useRegister() {
+  const router = useRouter();
+  const locale = useLocale();
 
-
-export function useRegister(){
-  const router = useRouter()
   const mutation = useMutation({
-    mutationFn :(payload : RegisterPayload) => registerApi(payload),
-    onSuccess: () =>
-      {
-      router.push("/auth/Login");
-             },
-  onError :(err : any) =>
-  {
-    console.error("Register failed", err?.message) 
-  },
+    mutationFn: (payload: RegisterPayload) => registerApi(payload),
 
-    });
-    return mutation;
-    
-  }
+    onSuccess: () => {
+  
+      router.push(`/${locale}/auth/Login`);
+    },
+
+    onError: (err: any) => {
+      console.error("Register failed", err?.message);
+    },
+  });
+
+  return mutation;
+}
