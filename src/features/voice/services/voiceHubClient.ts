0ac0f44318199496c4c233ecbfef122a4d9697a5
Ara_Mansouri <140816682+Ara-Mansouri@users.noreply.github.com.>
2025-11-10 12:19:03 +0330

    Test : Actual URL

--- src/features/voice/services/voiceHubClient.ts
+++ src/features/voice/services/voiceHubClient.ts
@@ -5,7 +5,7 @@ export function createVoiceHubConnection() {
   const baseUrl = process.env.NEXT_PUBLIC_API_URL;
 
   const connection = new signalR.HubConnectionBuilder()
-      .withUrl("/audioHub")
+      .withUrl("https://trading.liara.run/audioHub")
     .withAutomaticReconnect()
     .configureLogging(signalR.LogLevel.Information)
     .build();
