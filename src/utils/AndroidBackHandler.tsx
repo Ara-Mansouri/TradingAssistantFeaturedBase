a2105e0cda09950a5cb0033aa0bee86e74cd5757
Ara_Mansouri <140816682+Ara-Mansouri@users.noreply.github.com.>
2025-11-27 23:51:32 +0330

    Feat : Bug fix For Android Back Handler

--- src/utils/AndroidBackHandler.tsx
+++ src/utils/AndroidBackHandler.tsx
@@ -1,37 +1,43 @@
 "use client";
 
 import { App } from "@capacitor/app";
-import { useRouter } from "next/navigation";
+import { useRouter, usePathname } from "next/navigation";
 import { useEffect } from "react";
 
 export default function AndroidBackHandler() {
   const router = useRouter();
+  const pathname = usePathname();
 
   useEffect(() => {
     let handler: any;
 
-    const setup = async () => 
-    {
-      handler = await App.addListener("backButton", ({ canGoBack }) => 
-        {
+    const setup = async () => {
+      handler = await App.addListener("backButton", ({ canGoBack }) => {
 
-        if (!canGoBack) 
-        {
+        if (pathname === "/auth/Login") {
           App.exitApp();
           return;
         }
 
-        
-        router.back();
-       });
+        if (pathname === "/dashboard") {
+          return;
+        }
+
+        if (canGoBack) {
+          router.back();
+          return;
+        }
+
+        App.exitApp();
+      });
     };
 
     setup();
 
     return () => {
-      if (handler) handler.remove();
+      handler?.remove();
     };
-  }, [router]);
+  }, [pathname, router]);
 
   return null;
 }
