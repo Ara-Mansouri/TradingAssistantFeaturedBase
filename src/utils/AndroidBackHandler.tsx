04388387fbed1f004eadecad6d5dcc1a58682f04
Ara_Mansouri <140816682+Ara-Mansouri@users.noreply.github.com.>
2025-11-27 22:03:21 +0330

    Feat  ; Add Android Back Handler

---
+++ src/utils/AndroidBackHandler.tsx
@@ -0,0 +1,34 @@
+"use client";
+
+import { App } from "@capacitor/app";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function AndroidBackHandler() {
+  const router = useRouter();
+
+  useEffect(() => {
+    let handler: any;
+
+    const setup = async () => {
+      handler = await App.addListener("backButton", ({ canGoBack }) => {
+        if (!canGoBack) {
+          // اگر صفحه‌ای برای برگشت نیست، اپ را ببند
+          App.exitApp();
+          return;
+        }
+
+        
+        router.back();
+      });
+    };
+
+    setup();
+
+    return () => {
+      if (handler) handler.remove();
+    };
+  }, [router]);
+
+  return null;
+}
