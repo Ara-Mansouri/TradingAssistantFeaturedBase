8961638dcfa12300dccf17c72e9543bb78f2b536
Ara_Mansouri <140816682+Ara-Mansouri@users.noreply.github.com.>
2025-11-29 12:38:55 +0330

    Fix : Error Fix in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,9 @@
-"use client"
 import "./globals.css";
 import Providers from "@/lib/providers";
 import { Geist, Geist_Mono } from "next/font/google";
 import { NextIntlClientProvider } from "next-intl";
 import { headers } from "next/headers";
 import AndroidBackHandler from "@/utils/AndroidBackHandler"
-import { usePathname } from "next/navigation";
 
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
 const geistMono = Geist_Mono({ variable: "--font-geist-mono", subsets: ["latin"] });
@@ -26,14 +24,12 @@ export async function generateMetadata() {
 export default async function RootLayout({children,}: {  children: React.ReactNode;}) {
   const locale = (await headers()).get("x-next-intl-locale") || "en";
   const messages = (await import(`../i18n/messages/${locale}.json`)).default;
-    const  pathname  = usePathname();
   return (
     <html lang={locale} >
-      <head>
-  {pathname.startsWith("/auth") && (
-    <link rel="prefetch" href="/images/login-bg.jpg" as="image" />
-  )}
-</head>
+       <head>
+        {/* Prefetch the login background */}
+        <link rel="prefetch" href="/images/login-bg.jpg" as="image" />
+      </head>
       <body 
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#0b0b0b] text-white h-screen `}
       >
